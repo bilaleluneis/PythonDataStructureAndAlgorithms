9bde72f17af8cd3fb490a66b2f948c018955e1f1
Jieshu <foundwonder@gmail.com>
2018-08-01 16:31:05 -0400

    optimized the visualization codes.

--- insertion_sort.py
+++ insertion_sort.py
@@ -17,12 +17,10 @@ def insertion_sort_impl_b(list_to_sort: [int], visualize: bool = False) -> [int]
         while index_to_compare_from > 0:
             if result_list[index_to_compare_from] < result_list[index_to_compare_from - 1]:
                 swap(result_list, index_to_compare_from, (index_to_compare_from - 1))
+                if visualize:
+                    print(result_list)
             index_to_compare_from = index_to_compare_from - 1
-            if visualize:
-                print(result_list)
         current_index = current_index + 1
-        if visualize:
-            print(result_list)
     if visualize:
         print("After Sort: {}".format(result_list))
     return result_list
@@ -37,33 +35,30 @@ def swap(list_to_operate_on: [int], first_index, second_index):
 
 
 def insertion_sort_impl_j(list_to_be_sorted: [], visualize: bool = False) ->[]:
-    sorted_list = [list_to_be_sorted[0]]
+    result_list = [list_to_be_sorted[0]]
     if visualize:
         print("Before Sort: {}".format(list_to_be_sorted))
+        print(result_list)
     for element in list_to_be_sorted[1:]:
-        if element > sorted_list[-1]:
+        if element > result_list[-1]:
             sorted_list.append(element)
-        elif element < sorted_list[0]:
-            sorted_list.insert(0, element)
+        elif element < result_list[0]:
+            result_list.insert(0, element)
         else:
             for x in list(range(len(sorted_list) - 1)):
-                if sorted_list[x] <= element <= sorted_list[x+1]:
-                    sorted_list.insert(x+1, element)
-                    if visualize:
-                        print(sorted_list)
+                if result_list[x] <= element <= result_list[x+1]:
+                    result_list.insert(x+1, element)
                     break
-                if visualize:
-                    print(sorted_list)
         if visualize:
-            print(sorted_list)
+            print(result_list)
     if visualize:
-        print("After Sort: {}".format(sorted_list))
-    return sorted_list
+        print("After Sort: {}".format(result_list))
+    return result_list
 
 
 # start of running code
 if __name__ == "__main__":
     (sorted_list, random_list, reversed_list) = lists_generator(10, 10)
-    visualization_on: bool = False
+    visualization_on: bool = True
     analyze("Bilal's Insertion Sort", insertion_sort_impl_b, random_list, visualization_on)
     analyze("Jieshu's Insertion Sort", insertion_sort_impl_j, random_list, visualization_on)
