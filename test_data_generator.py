4061377bc6385ad2221dc47de827547a2fd060c2
bilaleluneis <bilaleluneis@gmail.com>
2018-08-01 09:52:54 -0400

    added more util functions, and a fix me to remove code in the future

--- test_data_generator.py
+++ test_data_generator.py
@@ -4,7 +4,7 @@ __since__ = "July 2018"
 __email__ = "bilaleluneis@gmail.com and foundwonder@gmail.com"
 
 from numpy import random as random_generator
-import time
+from time import time
 import collections
 
 
@@ -25,10 +25,46 @@ def lists_generator(end_number: int, list_size: int) -> ([int], [int], [int]):
 # returns array/list of integers and a float representing
 # the execution time of the function in milliseconds.
 def time_it(function_to_time: callable([int]), function_input: [int]) -> ([int], float):
-    start_time = float(time.time() * 1000)
+    start_time = float(time() * 1000)
     function_output = function_to_time(function_input)
-    end_time = float(time.time() * 1000)
+    end_time = float(time() * 1000)
     return function_output, (end_time - start_time)
 
 
+# takes two arrays/lists of integers and compare
+# each element with the other. will return True
+# if both arrays/lists are of same size and each
+# element on same index equal to the other.
+def compare_equal(list_one: [int], list_two: [int]) -> bool:
+
+    if len(list_one) != len(list_two):
+        return False
+
+    lists_size: int = len(list_one)  # doesnt matter which size you get, they both are same size
+    index: int = 0
+
+    if lists_size == 1:
+        return list_one[0] == list_two[0]
+
+    while index < lists_size:
+        if list_one[index] != list_two[index]:
+            return False
+        index = index + 1
+
+    return True  # if we get here , then we existed the loop without returning False, elements equal each other
+
+
+# makes a copy of list to sort, apply python's built in sort and return the copy sorted.
+# wrote this, because the default sort behaviour will sort the actual list passed.
+# we don't want that, so we can reuse the original list in other tests.
+def sort(list_to_sort: [int]) -> [int]:
+
+    if len(list_to_sort) <= 1:
+        return list_to_sort
+
+    sorted_list = list_to_sort
+    return sorted(sorted_list)
+
+
+# FIXME: get rid of the bellow if compare_equal works
 compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
