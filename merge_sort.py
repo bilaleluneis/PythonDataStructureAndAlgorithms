f9ff746fb483d62e72b7a00cd9bfe9e6659fce89
Jieshu <foundwonder@gmail.com>
2018-08-02 11:15:17 -0400

    change some print points

--- merge_sort.py
+++ merge_sort.py
@@ -35,12 +35,14 @@ def sort_merge_list(left_list: [int], right_list: [int]) ->[int]:
 # with one element, and then merging two adjacent lists.
 
 
-def merge_sort_bottom_up(list_to_be_sorted: [int]) -> [int]:
+def merge_sort_bottom_up(list_to_be_sorted: [int], visualize: bool = False) -> [int]:
     list_size = len(list_to_be_sorted)
     list_size_break_down = list_size
     list_break_down = list()
     for each_index in range(0, len(list_to_be_sorted)):
         list_break_down.append([list_to_be_sorted[each_index]])
+    if visualize:
+        print(list_break_down)
     while list_size_break_down > 1:
         current_index = 0
         while current_index < list_size_break_down - 1:
@@ -51,6 +53,8 @@ def merge_sort_bottom_up(list_to_be_sorted: [int]) -> [int]:
             del list_break_down[current_index + 1]
             current_index = current_index + 1
             list_size_break_down = len(list_break_down)
+            if visualize:
+                print(list_break_down)
     return list_break_down[0]
 
 
@@ -61,8 +65,8 @@ def merge_sort_bottom_up(list_to_be_sorted: [int]) -> [int]:
 # and then sorting and merging them back together.
 def merge_sort_recursion(list_to_be_sorted: [int], visualize: bool = False) ->[int]:
 
-    if visualize:
-        print(list_to_be_sorted)
+    if visualize and len(list_to_be_sorted) > 1:
+        print("list {} is broken down to".format(list_to_be_sorted))
 
     if len(list_to_be_sorted) == 1:
         return list_to_be_sorted
@@ -72,24 +76,24 @@ def merge_sort_recursion(list_to_be_sorted: [int], visualize: bool = False) ->[i
         left_list = list_to_be_sorted[:mid_index]
         right_list = list_to_be_sorted[mid_index:]
         if visualize:
-            print("recurse on the left list")
+            print("left list {} and right list {}".format(left_list, right_list))
+            print("recurse on the left list {}".format(left_list))
         left_list_sorted = merge_sort_recursion(left_list, visualize)
         if visualize:
-            print("recurse on the right list")
+            print("recurse on the right list {}".format(right_list))
         right_list_sorted = merge_sort_recursion(right_list, visualize)
         result_after_sort = sort_merge_list(left_list_sorted, right_list_sorted)
         if visualize:
-            print("list {} is break down to".format(list_to_be_sorted))
-            print("{} and {}".format(left_list, right_list))
-            print("sorted to {} and {}".format(left_list_sorted, right_list_sorted))
-            print("combined and sorted to {}".format(result_after_sort))
+            print("sort them respectively to {} and {}".format(left_list_sorted, right_list_sorted))
+            print("combine and sort them to {}".format(result_after_sort))
 
         return result_after_sort
 
 
 # start of running code
 if __name__ == "__main__":
-    (sorted_list, random_list, reversed_list) = lists_generator(50, 5)
+    (sorted_list, random_list, reversed_list) = lists_generator(20, 5)
     visualization_on: bool = True
     # analyze("Bilal's merge Sort", merge_sort_recursion_impl_j, random_list, visualization_on)
-    analyze("Jieshu's merge Sort", merge_sort_recursion, random_list, visualization_on)
+    analyze("Recursion merge Sort", merge_sort_recursion, random_list, visualization_on)
+    analyze("Jieshu's bottom up merge Sort", merge_sort_bottom_up, random_list, visualization_on)
