41c721eac31466e5c4f0ed7c731b89d4ced9bd6f
Jieshu <foundwonder@gmail.com>
2018-08-03 23:40:13 -0400

    initial commit

---
+++ functional_stack_data_structure.py
@@ -0,0 +1,22 @@
+
+__author__ = "Jieshu Wang"
+__since__ = "Aug 2018"
+__email__ = "foundwonder@gmail.com"
+
+
+stack = [int]
+
+
+def push(value: int) -> [int]:
+    pass
+
+
+def pop() -> int:
+    pass
+
+
+def create_a_new_array(value: [int]) -> [int]:
+
+
+# start of running code
+if __name__ == "__main__":
