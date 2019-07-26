d6ba889df8876661f18fe010e18348fcdfe67718
bilaleluneis <bilaleluneis@gmail.com>
2019-07-25 23:41:52 -0700

    update impl

--- abstract_array.py
+++ abstract_array.py
@@ -2,8 +2,7 @@ __author__ = "Jieshu Wang and Bilal El Uneis"
 __since__ = "Aug 2018"
 __email__ = "foundwonder@gmail.com and bilaleluneis@gmail.com"
 
-from typing import Optional
-
+from typing import Optional, Type, SupportsInt, List
 
 """
     This will eventually become a True Abstract class. for now
@@ -14,41 +13,48 @@ from typing import Optional
 
 class AbstractArray:
 
-    __internal_array: [int]  # array that will be used to store data for Stack instance
-
     def __init__(self):
-        self.__internal_array = [int] * 0
+        """
+        __internal_array is a list of type int or anything that supports type int
+        needed to add SupportsInt to clear warnings from PyCharm about
+        int() call that threw back that it expected type of SupportsInt and not
+        Type[int]
+        """
+        self.__internal_array: List[Type[int, SupportsInt]] = [int] * 0
 
     # returns a string representation of AbstractArray internal information
     def __str__(self):
-        return "{}".format(self.__internal_array)
+        discription: str = "["
 
-    def _get_current_size(self) -> int:
-        current_size = int(self.__size(self.__internal_array))
-        return current_size
+        for entry in self.__internal_array:
+            discription += "{}, ".format(entry)
 
-    def _get_value_at_index(self, index: int) -> Optional[int]:
-        if index < 0 or index >= self._get_current_size():
-            return None  # index provided falls out of range of the array!
-        else:
-            get_value = int(self.__internal_array[index])  # return a copy of that value and not reference!
-            return get_value
+        discription = discription[:-2]  # remove last space and last ','
+        discription += "]"
+        return discription
 
-    @staticmethod
-    def __size(an_array: [int]) -> int:
+    @property
+    def _size(self) -> int:
         array_size: int = 0
-        for _ in an_array:
-            array_size = array_size + 1
+        for _ in self.__internal_array:
+            array_size += 1
         return array_size
 
+    def _get_value_at_index(self, index: int) -> Optional[int]:
+        if index < 0 or index >= self._size:
+            return None  # index provided falls out of range of the array!
+        else:
+            get_value: Type[int, SupportsInt] = self.__internal_array[index]
+            return int(get_value)  # return a copy of that value and not reference!
+
     def _assign_value_to_index(self, value_to_insert: int, at_index: int):
-        if at_index < 0 or at_index >= self._get_current_size():
+        if at_index < 0 or at_index >= self._size:
             return
         else:
             self.__internal_array[at_index] = value_to_insert
 
     def _increase_array_size(self, by_number_of_rows: int):
-        original_array_size = self.__size(self.__internal_array)
+        original_array_size = self._size
         if by_number_of_rows < 1:
             return  # nothing to resize , just return
         elif original_array_size == 0:
@@ -58,20 +64,20 @@ class AbstractArray:
             index = 0
             while index < original_array_size:
                 new_array[index] = self.__internal_array[index]
-                index = index + 1
+                index += 1
             del self.__internal_array
             self.__internal_array = new_array
 
     def _decrease_array_size(self, by_number_of_rows: int):
-        original_array_size = self.__size(self.__internal_array)
+        original_array_size = self._size
         if (original_array_size - by_number_of_rows) <= 0:
             del self.__internal_array
             self.__internal_array = [int] * 0
         else:
             new_array = [int] * (original_array_size - by_number_of_rows)
             index = 0
-            while index < self.__size(new_array):
+            while index < self._size:
                 new_array[index] = self.__internal_array[index]
-                index = index + 1
+                index += 1
             del self.__internal_array
             self.__internal_array = new_array
--- object_stack_impl.py
+++ object_stack_impl.py
@@ -17,23 +17,15 @@ class Stack(AbstractArray):
         super().__init__()
         print("new instance of{} created! {}".format(object_type, self))
 
-    # just return AbstractArray __str__ , Stack has no internal properties.
-    def __str__(self):
-        return super().__str__()
-
-    def push(self, value: int, object_type: str = "Stack"):
-        print("pushing {} into {} {}".format(value, object_type, self))
+    def push(self, value: int):
         self._increase_array_size(by_number_of_rows=1)
-        self._assign_value_to_index(value, self._get_current_size() - 1)
-        print("{} after push {}".format(object_type, self))
+        self._assign_value_to_index(value, self._size - 1)
 
-    def pop(self, object_type: str = "Stack") -> Optional[int]:
-        current_size = self._get_current_size()
+    def pop(self) -> Optional[int]:
+        current_size = self._size
         if current_size > 0:
-            value_popped = self._get_value_at_index(self._get_current_size() - 1)
-            print("popping {} from {} {}".format(value_popped, object_type, self))
+            value_popped = self._get_value_at_index(self._size - 1)
             self._decrease_array_size(by_number_of_rows=1)
-            print("{} after pop {}".format(object_type, self))
             return value_popped
 
 
@@ -41,12 +33,15 @@ def main(num_push: int, num_pop: int):
     stack_instance = Stack()
 
     for i in range(num_push):
+        print("pushing {} into {} {}".format(i, "stack", stack_instance))
         stack_instance.push(i)
+        print("{} after push {}".format("stack", stack_instance))
 
     print()
 
     for _ in range(num_pop):
         value = stack_instance.pop()
+        print("{} after {} is poped {}".format("stack", value, stack_instance))
         del value  # just remove it from memory, don't really need it
 
     print()
